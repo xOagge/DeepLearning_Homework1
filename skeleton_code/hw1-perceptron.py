31d26bbae9528a4c371efac28eff5b3929bd4cb9
xOagge <jose.goncalo2004@gmail.com>
2025-11-28 22:05:49 +0000

    skeleton

---
+++ skeleton_code/hw1-perceptron.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+
+# Deep Learning Homework 1
+
+import argparse
+import time
+import pickle
+import json
+
+import numpy as np
+
+import utils
+
+
+class Perceptron:
+    def __init__(self, n_classes, n_features):
+        self.W = np.zeros((n_classes, n_features))
+
+    def save(self, path):
+        """
+        Save perceptron to the provided path
+        """
+        with open(path, "wb") as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def load(cls, path):
+        """
+        Load perceptron from the provided path
+        """
+        with open(path, "rb") as f:
+            return pickle.load(f)
+
+    def update_weight(self, x_i, y_i):
+        """
+        x_i (n_features,): a single training example
+        y_i (scalar): the gold label for that example
+        """
+        # Todo: Q1 1(a)
+        pass
+
+    def train_epoch(self, X, y):
+        """
+        X (n_examples, n_features): features for the whole dataset
+        y (n_examples,): labels for the whole dataset
+        """
+        # Todo: Q1 1(a)
+        pass
+
+    def predict(self, X):
+        """
+        X (n_examples, n_features)
+        returns predicted labels y_hat, whose shape is (n_examples,)
+        """
+        # Todo: Q1 1(a)
+        pass
+
+    def evaluate(self, X, y):
+        """
+        X (n_examples x n_features)
+        y (n_examples): gold labels
+
+        returns classifier accuracy
+        """
+        # Todo: Q1 1(a)
+        pass
+
+
+def main(args):
+    utils.configure_seed(seed=args.seed)
+
+    data = utils.load_dataset(data_path=args.data_path, bias=True)
+    X_train, y_train = data["train"]
+    X_valid, y_valid = data["dev"]
+    X_test, y_test = data["test"]
+    n_classes = np.unique(y_train).size
+    n_feats = X_train.shape[1]
+
+    # initialize the model
+    model = Perceptron(n_classes, n_feats)
+
+    epochs = np.arange(1, args.epochs + 1)
+
+    valid_accs = []
+    train_accs = []
+
+    start = time.time()
+
+    best_valid = 0.0
+    best_epoch = -1
+    for i in epochs:
+        print('Training epoch {}'.format(i))
+        train_order = np.random.permutation(X_train.shape[0])
+        X_train = X_train[train_order]
+        y_train = y_train[train_order]
+
+        model.train_epoch(X_train, y_train)
+
+        train_acc = model.evaluate(X_train, y_train)
+        valid_acc = model.evaluate(X_valid, y_valid)
+
+        train_accs.append(train_acc)
+        valid_accs.append(valid_acc)
+
+        print('train acc: {:.4f} | val acc: {:.4f}'.format(train_acc, valid_acc))
+
+        # Todo: Q1(a)
+        # Decide whether to save the model to args.save_path based on its
+        # validation score
+
+    elapsed_time = time.time() - start
+    minutes = int(elapsed_time // 60)
+    seconds = int(elapsed_time % 60)
+    print('Training took {} minutes and {} seconds'.format(minutes, seconds))
+
+    print("Reloading best checkpoint")
+    best_model = Perceptron.load(args.save_path)
+    test_acc = best_model.evaluate(X_test, y_test)
+
+    print('Best model test acc: {:.4f}'.format(test_acc))
+
+    utils.plot(
+        "Epoch", "Accuracy",
+        {"train": (epochs, train_accs), "valid": (epochs, valid_accs)},
+        filename=args.accuracy_plot
+    )
+
+    with open(args.scores, "w") as f:
+        json.dump(
+            {"best_valid": float(best_valid),
+             "selected_epoch": int(best_epoch),
+             "test": float(test_acc),
+             "time": elapsed_time},
+            f,
+            indent=4
+        )
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epochs', default=20, type=int,
+                        help="""Number of epochs to train for.""")
+    parser.add_argument('--data-path', type=str, default="emnist-letters.npz")
+    parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--save-path", required=True)
+    parser.add_argument("--accuracy-plot", default="Q1-perceptron-accs.pdf")
+    parser.add_argument("--scores", default="Q1-perceptron-scores.json")
+    args = parser.parse_args()
+    main(args)
